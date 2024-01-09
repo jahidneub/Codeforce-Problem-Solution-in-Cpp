837fe85018f3347dfffcf03c5b39578f6cf5ec62
Jahid Hasan <153955082+jahidneub@users.noreply.github.com>
2024-01-10 05:36:18 +0600

    Add files via upload

---
+++ 4A Watermelon .cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w;
+    if(1<=w<=100)
+    {
+        if(w==2){printf("NO\n");}
+
+        else if (w%2==0){ printf("YES\n");}
+
+         else  {printf("NO\n");}
+
+    }
+    return 0;
+}
