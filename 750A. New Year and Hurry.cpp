2772dcdecb74b0b2133f2e04db7e39e7c55ee7d2
Jahid Hasan <153955082+jahidneub@users.noreply.github.com>
2024-01-22 02:57:51 +0600

    Add files via upload

---
+++ 750A. New Year and Hurry.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k,i,sum,res,c;
+
+    while (cin >> n >> k)
+    {
+        res = 240 - k, sum = 0, c = 0;
+
+        for (i=1; i<=n; i++)
+        {
+            sum += 5 * i;
+
+            if (sum > res)
+                break;
+
+            c = c + 1;
+        }
+
+        cout << c << endl;
+    }
+
+
+}
